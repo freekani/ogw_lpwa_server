1f022b1f40683355291e1d305eadb26d188afc30
Nao Tomori <@tomorinao>
2020-07-17 01:45:33 +0900

    modified:   server/Models/CallbackForSigfoxModel.cs 	modified:   server/Models/ReturnForSigfoxModel.cs

--- server/Models/CallbackForSigfoxModel.cs
+++ server/Models/CallbackForSigfoxModel.cs
@@ -1,11 +1,10 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 [Serializable]
 public class CallbackForSigfoxModel
 {
-    //[Key]
-    //public string DeviceId ="";
+    [Key]
+    public string DeviceId { get; set; }
     [Key]
     public string Data { get; set; }
 }
--- server/Models/ReturnForSigfoxModel.cs
+++ server/Models/ReturnForSigfoxModel.cs
@@ -1,11 +1,12 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-
+using System.Text.Json.Serialization;
 
 [Serializable]
 public class ReturnForSigfoxModel
 {
-    [Key]
+    //[Key]
+    [JsonIgnore]
     public string deviceId{get;set;}
     [Key]
     public string downlinkData { get; set; }
