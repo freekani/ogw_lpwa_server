2e40121a6656ee1fb5030f14242bbcdb02f6a3d0
Nao Tomori <@tomorinao>
2020-07-17 01:28:30 +0900

    modified:   server/Models/CallbackForSigfoxModel.cs

--- server/Models/CallbackForSigfoxModel.cs
+++ server/Models/CallbackForSigfoxModel.cs
@@ -3,8 +3,8 @@ using System.ComponentModel.DataAnnotations;
 [Serializable]
 public class CallbackForSigfoxModel
 {
-    [Key]
-    public string DeviceId { get; set; }
+    //[Key]
+    //public string DeviceId { get; set; }
     [Key]
     public string Data { get; set; }
 }
