5a70ef65c1dec83782d97a9b1f5e2384cf7d1146
Nao Tomori <@tomorinao>
2020-07-17 01:40:49 +0900

    modified:   server/Models/CallbackForSigfoxModel.cs

--- server/Models/CallbackForSigfoxModel.cs
+++ server/Models/CallbackForSigfoxModel.cs
@@ -4,8 +4,9 @@ using System.Text.Json.Serialization;
 [Serializable]
 public class CallbackForSigfoxModel
 {
-    [JsonIgnore]
-    public string DeviceId { get; set; }
+
+    [NonSerialized]
+    public string DeviceId ="";
     [Key]
     public string Data { get; set; }
 }
